c3d65ec851321be964ba52f918ce9428588ac05c
matthieu88160 <matthieu.vallance@cscfa.fr>
2016-12-02 23:57:21 +0100

    resolving dependencies

--- before_php.sh
+++ before_php.sh
@@ -2,16 +2,18 @@
 
 apt-get update
 
-apt-get install -y apt-utils \
-	build-essential
+apt-get install -y apt-utils 
+apt-get install -y build-essential
 
-apt-get install -y curl \
-	libcurl3 \
-	libcurl3-dev \
-	libjpeg-dev \
-	libpng-dev \
-	libgcrypt11-dev \
-	zlib1g-dev \
-	libmcrypt-dev \
-	libxml2-dev \
-	pkg-config
+apt-get install -y curl
+apt-get install -y libcurl3 
+apt-get install -y libcurl3-dev 
+apt-get install -y libjpeg-dev 
+apt-get install -y libpng-dev 
+apt-get install -y libgcrypt11-dev 
+apt-get install -y zlib1g-dev 
+apt-get install -y libmcrypt-dev 
+apt-get install -y libxml2-dev 
+apt-get install -y pkg-config 
+apt-get install -y libssl-dev
+apt-get install -y libicu-dev
