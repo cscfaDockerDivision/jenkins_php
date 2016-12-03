3af56a258430568b41303efbe73b7c950d114477
matthieu88160 <matthieu.vallance@cscfa.fr>
2016-12-03 01:54:12 +0100

    Resolve typo

--- composer.sh
+++ composer.sh
@@ -5,5 +5,5 @@ wget https://getcomposer.org/installer -P /usr/local/src/composer/
 mv /usr/local/src/composer/installer /usr/local/src/composer/composer-setup.php
 cd /usr/local/src/composer/
 php70 composer-setup.php
-rm composer.setup.php
+rm composer_setup.php
 ln -s /usr/local/src/composer/composer.phar /usr/local/bin/composer
